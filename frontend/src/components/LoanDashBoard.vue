a59fd9cb785ac28112dc3b9769fd3993dd2d695f
yesslee31 <yesslee31@gmail.com>
2024-11-17 16:11:54 +0900

    update: 소비 현황 캘린더 수정

--- frontend/src/components/LoanDashBoard.vue
+++ frontend/src/components/LoanDashBoard.vue
@@ -2,93 +2,127 @@
   <div class="dashboard">
     <h2>KB 비상금 대출</h2>
     <div class="body">
-    <div class="progress-container">
-      <div class="progress-label">
-        <span>0월 상환 진행률</span>
-        <span>1/6 개월</span>
-        <span>{{ progressPercentage }}%</span>
-      </div>
-      <div class="progress-bar">
-        <div class="progress" :style="{ width: progressPercentage + '%' }"></div>
-      </div>
-      <div class="amounts">
-        <span>{{ currentAmount }}원</span>
-        <span>/ {{ totalAmount }}원</span>
-      </div>
-    </div>
-    <section class="summary">
-      <div class="summary-item">
-        <span>목표 상환 원금</span>
-        <span>{{ targetAmount }}원</span>
-      </div>
-      <div class="summary-item">
-        <span>이자 납입액</span>
-        <span>{{ interestAmount }}원</span>
-      </div>
-      <div class="summary-total">
-        <span>총 납부액</span>
-        <span>{{ totalPayment }}원</span>
-      </div>
-    </section>
-    <section class="calendar">
-      <div class="financial-info">
-        <p class="expense">지출: {{ expenditure }}원</p>
-        <p class="income">수입: {{ income }}원</p>
-      </div>
-      <vue-cal
-        small
-        v-model="events"
-        :hide-view-selector="true"
-        :time="false"
-        :active-view="'month'"
-        :events="events"
-        :event-count-on-month-view
-        :style="{ border: 'none' }">
-        <template #events-count="{ events }">
-          <span v-if="events.length">
-            {{ events.length }} 이벤트
-          </span>
-        </template>
-      </vue-cal>
-    </section>
-    <section class="plan">
-      <h2>식비 절약 플랜</h2>
-      <div class="budget-container">
-        <div class="budget-bar">
-          <div class="budget-segment" :style="{ width: (budgetItems[0].budget / totalBudget) * 100 + '%', backgroundColor: '#f44336' }">식비</div>
-          <div class="budget-segment" :style="{ width: (budgetItems[1].budget / totalBudget) * 100 + '%', backgroundColor: '#ffcc00' }">쇼핑비</div>
-          <div class="budget-segment" :style="{ width: (budgetItems[2].budget / totalBudget) * 100 + '%', backgroundColor: '#4caf50' }">여가비</div>
+      <div class="progress-container">
+        <div class="progress-label">
+          <span>0월 상환 진행률</span>
+          <span>1/6 개월</span>
+          <span>{{ progressPercentage }}%</span>
         </div>
-        <div class="change" @click="toggleEditMode">
-          비율 변경 >
-        </div>
-      </div>
-      <div class="category" v-for="(item, index) in budgetItems" :key="index">
-        <div class="category-header">
-          <div class="category-name">{{ item.name }}</div>
-          <div :class="['status', getStatusClass(item.status)]">{{ getStatusText(item.status) }}</div>
-        </div>
-        <div class="category-bar">
+        <div class="progress-bar">
           <div
             class="progress"
-            :style="{ width: (item.spent / item.budget) * 100 + '%' }"
-            :class="item.status"
+            :style="{ width: progressPercentage + '%' }"
           ></div>
         </div>
         <div class="amounts">
-          <span>{{ item.spent }}원</span>
-          <span>/ {{ item.budget }}원</span>
+          <span>{{ currentAmount }}원</span>
+          <span>/ {{ totalAmount }}원</span>
         </div>
       </div>
-    </section>
-  </div>
+      <section class="summary">
+        <div class="summary-item">
+          <span>목표 상환 원금</span>
+          <span>{{ targetAmount }}원</span>
+        </div>
+        <div class="summary-item">
+          <span>이자 납입액</span>
+          <span>{{ interestAmount }}원</span>
+        </div>
+        <div class="summary-total">
+          <span>총 납부액</span>
+          <span>{{ totalPayment }}원</span>
+        </div>
+      </section>
+      <section class="calendar">
+        <div class="financial-info">
+          <p class="expense">지출: {{ expenditure }}원</p>
+          <p class="income">수입: {{ income }}원</p>
+        </div>
+        <vue-cal
+          xsmall
+          v-model="events"
+          :time="false"
+          hide-view-selector
+          active-view="month"
+          :disable-views="['years', 'year', 'week', 'day']"
+          :events="events"
+          events-on-month-view="short"
+          class="vuecal--yellow-theme"
+          style="height: 400px"
+        >
+          <!-- <template
+            ><div
+              v-for="event in events"
+              :key="event.start"
+              :class="getEventClass(event.content)"
+            >
+              <span>{{ event.title }}</span>
+            </div></template
+          > -->
+        </vue-cal>
+      </section>
+      <section class="plan">
+        <h2>식비 절약 플랜</h2>
+        <div class="budget-container">
+          <div class="budget-bar">
+            <div
+              class="budget-segment"
+              :style="{
+                width: (budgetItems[0].budget / totalBudget) * 100 + '%',
+                backgroundColor: '#f44336',
+              }"
+            >
+              식비
+            </div>
+            <div
+              class="budget-segment"
+              :style="{
+                width: (budgetItems[1].budget / totalBudget) * 100 + '%',
+                backgroundColor: '#ffcc00',
+              }"
+            >
+              쇼핑비
+            </div>
+            <div
+              class="budget-segment"
+              :style="{
+                width: (budgetItems[2].budget / totalBudget) * 100 + '%',
+                backgroundColor: '#4caf50',
+              }"
+            >
+              여가비
+            </div>
+          </div>
+          <div class="change" @click="toggleEditMode">비율 변경 ></div>
+        </div>
+        <div class="category" v-for="(item, index) in budgetItems" :key="index">
+          <div class="category-header">
+            <div class="category-name">{{ item.name }}</div>
+            <div :class="['status', getStatusClass(item.status)]">
+              {{ getStatusText(item.status) }}
+            </div>
+          </div>
+          <div class="category-bar">
+            <div
+              class="progress"
+              :style="{ width: (item.spent / item.budget) * 100 + '%' }"
+              :class="item.status"
+            ></div>
+          </div>
+          <div class="amounts">
+            <span>{{ item.spent }}원</span>
+            <span>/ {{ item.budget }}원</span>
+          </div>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import VueCal from 'vue-cal';
-import '../../node_modules/vue-cal/dist/vuecal.css'; // Ensure you import the CSS
+import { ref } from "vue";
+import VueCal from "vue-cal";
+import "../../node_modules/vue-cal/dist/vuecal.css"; // Ensure you import the CSS
 
 const currentAmount = 530000; // 현재 금액
 const totalAmount = 3000000; // 총 목표 금액
@@ -100,49 +134,68 @@ const expenditure = 1488100; // 지출
 const income = 2835502; // 수입
 
 const events = ref([
-  { start: '2024-11-19', end: '2024-11-19', title: '지출: 50,000원', color: '#f44336' }, // Expenditure
-  { start: '2024-11-20', end: '2024-11-20', title: '수입: 100,000원', color: '#4caf50' }, // Income
+  {
+    start: "2024-11-19",
+    end: "2024-11-19",
+    title: "-50,000",
+    content: "expenditure",
+    class: "expenditure-event",
+  }, // Expenditure
+  {
+    start: "2024-11-20",
+    end: "2024-11-20",
+    title: "+100,000",
+    content: "income",
+    class: "income-event",
+  }, // Income
+  {
+    start: "2024-11-20",
+    end: "2024-11-20",
+    title: "-30,000",
+    content: "expenditure",
+    class: "expenditure-event",
+  }, // Expenditure
   // Add more events as needed
 ]);
 
 const budgetItems = [
-  { name: '식비', spent: 70000, budget: 200000, status: 'safe' },
-  { name: '쇼핑비', spent: 100000, budget: 150000, status: 'danger' },
-  { name: '여가비', spent: 50000, budget: 280000, status: 'warning' },
+  { name: "식비", spent: 70000, budget: 200000, status: "safe" },
+  { name: "쇼핑비", spent: 100000, budget: 150000, status: "danger" },
+  { name: "여가비", spent: 50000, budget: 280000, status: "warning" },
 ];
 
 // Function to get status text
 const getStatusText = (status) => {
   switch (status) {
-    case 'safe':
-      return '안전';
-    case 'danger':
-      return '위험';
-    case 'warning':
-      return '적정';
+    case "safe":
+      return "안전";
+    case "danger":
+      return "위험";
+    case "warning":
+      return "적정";
     default:
-      return '';
+      return "";
   }
 };
 
 // Function to get status class
 const getStatusClass = (status) => {
   switch (status) {
-    case 'safe':
-      return 'status-safe'; 
-    case 'danger':
-      return 'status-danger'; 
-    case 'warning':
-      return 'status-warning'; 
+    case "safe":
+      return "status-safe";
+    case "danger":
+      return "status-danger";
+    case "warning":
+      return "status-warning";
     default:
-      return '';
+      return "";
   }
 };
 
 const totalBudget = budgetItems.reduce((sum, item) => sum + item.budget, 0);
 </script>
 
-<style scoped>
+<style>
 .dashboard {
   text-align: center;
 }
@@ -202,9 +255,61 @@ const totalBudget = budgetItems.reduce((sum, item) => sum + item.budget, 0);
   margin: 20px 0;
 }
 
+/* vuecal Yellow theme */
+.vuecal--yellow-theme .vuecal__menu,
+.vuecal--yellow-theme .vuecal__cell-events-count {
+  background-color: rgba(255, 179, 0, 0.8);
+}
+
+.vuecal--yellow-theme .vuecal__menu {
+  color: #fff;
+}
+
+.vuecal--yellow-theme .vuecal__title-bar {
+  background-color: rgba(255, 179, 0, 0.8);
+}
+
+.vuecal--yellow-theme .vuecal__cell--today,
+.vuecal--yellow-theme .vuecal__cell--current {
+  background-color: rgba(255, 236, 202, 0.4);
+}
+
+.vuecal--yellow-theme .vuecal__cell--selected {
+  background-color: rgba(255, 179, 0, 0.4);
+}
+
+.vuecal--yellow-theme .vuecal__cell--selected:before {
+  border-color: rgba(235, 216, 182, 0.5);
+}
+
+.vuecal--yellow-theme .vuecal__event-title {
+  padding: 0 2px;
+  font-size: 0.6em;
+}
+
+/* Uncomment the following block if you want the event background color */
+.vuecal--yellow-theme .vuecal__event {
+  /* background-color: #ffd54f; */
+  padding: 0 2px;
+  font-size: 0.85em;
+}
+
+.expenditure-event {
+  color: red;
+}
+
+.income-event {
+  color: blue;
+}
+
+/* .vuecal--yellow-theme .vuecal__event-title {
+  padding: 0 2px;
+  font-size: 0.6em;
+} */
+
 .financial-info {
   display: flex;
-  justify-content: flex-end; 
+  justify-content: flex-end;
   margin: 20px 0;
   color: #ecb91f;
   font-weight: bold;
@@ -220,17 +325,17 @@ const totalBudget = budgetItems.reduce((sum, item) => sum + item.budget, 0);
 
 .budget-container {
   display: flex;
-  align-items: center; 
-  justify-content: space-between; 
+  align-items: center;
+  justify-content: space-between;
 }
 
 .budget-bar {
   display: flex;
-  height: 30px; 
+  height: 30px;
   border-radius: 5px;
   margin-bottom: 20px;
   margin-top: 20px;
-  width: 70%; 
+  width: 70%;
 }
 
 .budget-segment {
@@ -238,14 +343,14 @@ const totalBudget = budgetItems.reduce((sum, item) => sum + item.budget, 0);
   display: flex;
   align-items: center;
   justify-content: center;
-  color: white; 
+  color: white;
   font-weight: bold;
 }
 
 .change {
-  margin-left: 10px; 
+  margin-left: 10px;
   color: rgb(48, 110, 243);
-  cursor: pointer; 
+  cursor: pointer;
 }
 
 .category {
@@ -254,7 +359,7 @@ const totalBudget = budgetItems.reduce((sum, item) => sum + item.budget, 0);
 
 .category-header {
   display: flex;
-  justify-content: space-between; 
+  justify-content: space-between;
   align-items: center;
 }
 
@@ -268,20 +373,20 @@ const totalBudget = budgetItems.reduce((sum, item) => sum + item.budget, 0);
   font-size: 14px;
   margin-bottom: 5px;
   padding: 5px 10px;
-  border-radius: 10px; 
-  color: rgb(255, 255, 255); 
+  border-radius: 10px;
+  color: rgb(255, 255, 255);
 }
 
 .status-safe {
-  background-color: #4caf50; 
+  background-color: #4caf50;
 }
 
 .status-danger {
-  background-color: #f44336; 
+  background-color: #f44336;
 }
 
 .status-warning {
-  background-color: #ffcc00; 
+  background-color: #ffcc00;
 }
 
 .category-bar {
@@ -302,5 +407,4 @@ const totalBudget = budgetItems.reduce((sum, item) => sum + item.budget, 0);
   justify-content: space-between;
   font-size: 14px;
 }
-
 </style>
