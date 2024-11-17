78154fe3684da60f0c7e1bae0436cd773e3b1368
subinPark <subin4420@gmail.com>
2024-11-17 11:58:37 +0900

    add: requirements, init, config

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,4 +1,5 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # MySQL 연결 정보
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:1234@localhost/notLoanly'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # SQLAlchemy 이벤트 시스템 비활성화
     SECRET_KEY = "your-secret-key"
