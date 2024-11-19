f93c2092a696c42639bad2ce23b96cfca0af21c3
s-eun20 <sdf3872@gmail.com>
2024-11-19 17:47:55 +0900

    fix : 백엔드 연결

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,7 +4,7 @@
 #     SECRET_KEY = "your-secret-key"
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:1220@localhost:3306/notLoanly"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://admin:1234@localhost:3306/notLoanly"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "a_very_secret_key_that_should_not_be_shared"
 
