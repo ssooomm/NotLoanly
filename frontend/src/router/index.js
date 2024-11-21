2e3e341f14477dd6720df05ef742afb3e88b059a
yesslee31 <yesslee31@gmail.com>
2024-11-21 22:27:45 +0900

    fix: 현재 상환 데이터 가져오는 부분 수정

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,17 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import MainView from '../views/MainView.vue';
 import LoanInfoNotice from '../views/loanInfo/LoanInfoNotice.vue';
-// import LoanInfoCollection from '../views/LoanInfoCollection.vue';
 import LoanInput from '../views/LoanInput.vue';
 import LoanComplete from '../views/LoanComplete.vue';
-// import ExpenseAnalysis from '../views/ExpenseAnalysis.vue';
 import RepaymentPlanSuggestion from '../views/plans/RepaymentPlanSuggestion.vue';
 import RepaymentPlanSuggestionDetail from '../views/plans/RepaymentPlanSuggestionDetail.vue';
 import ExpenseAnalysis from '../views/ExpenseAnalysis.vue';
-// import RepaymentDashboardSummary from '../views/RepaymentDashboardSummary.vue';
-// import RepaymentDashboardExpenseRatioChange from '../views/RepaymentDashboardExpenseRatioChange.vue';
-// import RepaymentStatus from '../views/RepaymentStatus.vue';
-// import RepaymentDashboardExpenseAnalysis from '../views/RepaymentDashboardExpenseAnalysis.vue';
 import Notifications from '../views/Notifications.vue';
 
 const routes = [
@@ -25,11 +19,6 @@ const routes = [
     name: 'LoanInfoNotice',
     component: LoanInfoNotice,
   },
-  // {
-  //   path: '/loan-info-collection',
-  //   name: 'LoanInfoCollection',
-  //   component: LoanInfoCollection,
-  // },
   {
     path: '/loan-input',
     name: 'LoanInput',
@@ -39,6 +28,13 @@ const routes = [
     path: '/loan-complete',
     name: 'LoanComplete',
     component: LoanComplete,
+    beforeEnter: (to, from, next) => {
+      if (from.name === 'LoanInput') {
+        next();
+      } else {
+        next('/loan-input');
+      }
+    },
   },
   {
     path: '/expense-analysis',
@@ -55,26 +51,6 @@ const routes = [
     name: 'RepaymentPlanSuggestionDetail',
     component: RepaymentPlanSuggestionDetail,
   },
-  // {
-  //   path: '/repayment-dashboard-summary',
-  //   name: 'RepaymentDashboardSummary',
-  //   component: RepaymentDashboardSummary,
-  // },
-  // {
-  //   path: '/repayment-dashboard-expense-ratio-change',
-  //   name: 'RepaymentDashboardExpenseRatioChange',
-  //   component: RepaymentDashboardExpenseRatioChange,
-  // },
-  // {
-  //   path: '/repayment-status',
-  //   name: 'RepaymentStatus',
-  //   component: RepaymentStatus,
-  // },
-  // {
-  //   path: '/repayment-dashboard-expense-analysis',
-  //   name: 'RepaymentDashboardExpenseAnalysis',
-  //   component: RepaymentDashboardExpenseAnalysis,
-  // },
   {
     path: '/notifications',
     name: 'Notifications',
